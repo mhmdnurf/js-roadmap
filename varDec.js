b41d964182536a8845417e124706295b50958cea
Muhammad Nurfatkhur Rahman <mhmd.nurfat@gmail.com>
2023-03-22 20:08:27 +0700

    materi baru

---
+++ varDec.js
@@ -0,0 +1,17 @@
+// const buttonA = document.querySelector("#button_A");
+// const headingA = document.querySelector("#heading_A");
+
+// buttonA.onclick = () => {
+//   const name = prompt("What is your name?");
+//   alert(`Hello ${name}, nice to see you!`);
+//   headingA.textContent = `Welcome ${name}`;
+// };
+
+myName = "Chris";
+const logName = () => {
+  console.log(myName);
+};
+
+logName();
+
+// var myName;
